8d44cdc77b8a92dc5a2b133466d5986b6e71b38d
zachary.drake <zach.drake@citycharmers.com>
2023-05-07 22:31:04 -0400

    beginning course

---
+++ lib/models/expense.dart
@@ -0,0 +1,8 @@
+class Expense{
+  Expense({required this.title, required this.amount, required this.date});
+
+  //final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
